d7a4a36fb493a92e2799a06795dd96608e1f91bf
JohanSaisa <johan.saisa@gmail.com>
2022-03-21 16:06:07 +0100

    Draft placeholder pages for reference. Add menu item links. #25.

--- src/GT.UI/Controllers/HomeController.cs
+++ src/GT.UI/Controllers/HomeController.cs
@@ -4,29 +4,44 @@ using System.Diagnostics;
 
 namespace GT.UI.Controllers
 {
-	public class HomeController : Controller
-	{
-		private readonly ILogger<HomeController> _logger;
+  public class HomeController : Controller
+  {
+    private readonly ILogger<HomeController> _logger;
 
-		public HomeController(ILogger<HomeController> logger)
-		{
-			_logger = logger;
-		}
+    public HomeController(ILogger<HomeController> logger)
+    {
+      _logger = logger;
+    }
 
-		public IActionResult Index()
-		{
-			return View();
-		}
+    public IActionResult Index()
+    {
+      return View();
+    }
 
-		public IActionResult Privacy()
-		{
-			return View();
-		}
+    public IActionResult Contact()
+    {
+      return View();
+    }
 
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
-	}
+    public IActionResult Listings()
+    {
+      return View();
+    }
+
+    public IActionResult About()
+    {
+      return View();
+    }
+
+    public IActionResult Privacy()
+    {
+      return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+  }
 }
