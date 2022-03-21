46f54f09b80bde0a9d4aaebb3be715911151c4e0
Rasmus Broborg <43502647+RasmusBroborg@users.noreply.github.com>
2022-03-21 17:33:45 +0100

    Normalize Address Table. Remove unnessecary migrations.

--- src/GT.Data/Data/GTAppDb/Entities/Address.cs
+++ src/GT.Data/Data/GTAppDb/Entities/Address.cs
@@ -1,21 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GT.Data.Data.GTAppDb.Entities
 {
-  internal class Address : IGTEntity
-  {
-    [Column(TypeName = "nvarchar(450)")]
-    public string Id { get; set; }
+	internal class Address : IGTEntity
+	{
+		[Column(TypeName = "nvarchar(450)")]
+		public string Id { get; set; }
 
-    [Column(TypeName = "nvarchar(200)")]
-    public string StreetAddress { get; set; }
+		[Column(TypeName = "nvarchar(200)")]
+		public string StreetAddress { get; set; }
 
-    [Column(TypeName = "varchar(5)")]
-    public string ZipCode { get; set; }
-  }
+		[Column(TypeName = "varchar(5)")]
+		public string ZipCode { get; set; }
+
+		public ICollection<Company> Companies { get; set; }
+	}
 }
