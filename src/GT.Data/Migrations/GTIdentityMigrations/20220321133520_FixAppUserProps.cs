0a57d79db1927582e6c61c304c46a1145cf37b0e
Rasmus Broborg <43502647+RasmusBroborg@users.noreply.github.com>
2022-03-21 14:36:40 +0100

    Add data annotations set to nvarcharmax(70) to name properties. #19.

---
+++ src/GT.Data/Migrations/GTIdentityMigrations/20220321133520_FixAppUserProps.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GT.Data.Migrations.GTIdentityMigrations
+{
+    public partial class FixAppUserProps : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "nvarchar(70)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "nvarchar(70)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "AspNetUsers");
+        }
+    }
+}
