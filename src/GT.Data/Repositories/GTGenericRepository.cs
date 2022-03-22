4fc8e8d519d43e7ae4c6b16e8af3ff40591dd460
Rasmus Broborg <43502647+RasmusBroborg@users.noreply.github.com>
2022-03-22 15:53:45 +0100

    Add empty constructor to Generic Repository. #32.

--- src/GT.Data/Repositories/GTGenericRepository.cs
+++ src/GT.Data/Repositories/GTGenericRepository.cs
@@ -5,165 +5,170 @@ using System.Linq.Expressions;
 
 namespace GT.Data.Repositories
 {
-  /// <summary>
-  /// Base repository class.
-  /// </summary>
-  /// <typeparam name="TEntity">The database entity handled by the repository.</typeparam>
-  public class GTGenericRepository<TEntity>
-  : IGTGenericRepository<TEntity>, IDisposable
-      where TEntity : class, IGTEntity
-  {
-    protected readonly DbContext _context;
-    protected bool _disposed;
+	/// <summary>
+	/// Base repository class.
+	/// </summary>
+	/// <typeparam name="TEntity">The database entity handled by the repository.</typeparam>
+	public class GTGenericRepository<TEntity>
+	: IGTGenericRepository<TEntity>, IDisposable
+			where TEntity : class, IGTEntity
+	{
+		protected readonly DbContext _context;
+		protected bool _disposed;
 
-    public GTGenericRepository(DbContext context)
-    {
-      _context = context;
-    }
+		public GTGenericRepository(DbContext context)
+		{
+			_context = context;
+		}
 
-    /// <summary>
-    /// Gets all entities of type TEntity from the database, with optional included related data.
-    /// </summary>
-    /// <param name="includeExpression">Entity related data to be included. Example:
-    /// <br>
-    /// var customers = GetAll(c => c.Include(c.Address).ThenInclude(a => a.City));
-    /// </br></param>
-    /// <returns>IQueryable containing all database entities.</returns>
-    public virtual IQueryable<TEntity> GetAll(
-        Expression<Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>>? includeExpression = null)
-    {
-      // Check if any related data is to be included.
-      if (includeExpression is not null)
-      {
-        // Compile the include expression and apply it to the relevant DbSet.
-        var include = includeExpression.Compile();
-        return include(_context.Set<TEntity>());
-      }
+		public GTGenericRepository()
+		{
 
-      return _context.Set<TEntity>();
-    }
+		}
 
-    /// <summary>
-    /// Finds a database entity by a set of keys. More efficient than finding by predicate, but cannot include related data.
-    /// </summary>
-    /// <param name="keys"></param>
-    /// <returns></returns>
-    public async Task<TEntity> FindAsync(params object[] keys)
-    {
-      return await _context
-        .Set<TEntity>()
-        .FindAsync(keys);
-    }
+		/// <summary>
+		/// Gets all entities of type TEntity from the database, with optional included related data.
+		/// </summary>
+		/// <param name="includeExpression">Entity related data to be included. Example:
+		/// <br>
+		/// var customers = GetAll(c => c.Include(c.Address).ThenInclude(a => a.City));
+		/// </br></param>
+		/// <returns>IQueryable containing all database entities.</returns>
+		public virtual IQueryable<TEntity> GetAll(
+				Expression<Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>>? includeExpression = null)
+		{
+			// Check if any related data is to be included.
+			if (includeExpression is not null)
+			{
+				// Compile the include expression and apply it to the relevant DbSet.
+				var include = includeExpression.Compile();
+				return include(_context.Set<TEntity>());
+			}
 
-    /// <summary>
-    /// Adds a new entity to the database.
-    /// </summary>
-    /// <param name="entity"></param>
-    /// <returns></returns>
-    /// <exception cref="ArgumentNullException"></exception>
-    public async Task<TEntity> AddAsync(TEntity entity)
-    {
-      if (entity is null)
-      {
-        throw new ArgumentNullException(nameof(entity));
-      }
+			return _context.Set<TEntity>();
+		}
 
-      try
-      {
-        await _context
-          .Set<TEntity>()
-          .AddAsync(entity);
+		/// <summary>
+		/// Finds a database entity by a set of keys. More efficient than finding by predicate, but cannot include related data.
+		/// </summary>
+		/// <param name="keys"></param>
+		/// <returns></returns>
+		public async Task<TEntity> FindAsync(params object[] keys)
+		{
+			return await _context
+				.Set<TEntity>()
+				.FindAsync(keys);
+		}
 
-        await _context.SaveChangesAsync();
-      }
-      catch (DbUpdateException)
-      {
-        throw;
-      }
+		/// <summary>
+		/// Adds a new entity to the database.
+		/// </summary>
+		/// <param name="entity"></param>
+		/// <returns></returns>
+		/// <exception cref="ArgumentNullException"></exception>
+		public async Task<TEntity> AddAsync(TEntity entity)
+		{
+			if (entity is null)
+			{
+				throw new ArgumentNullException(nameof(entity));
+			}
 
-      return entity;
-    }
+			try
+			{
+				await _context
+					.Set<TEntity>()
+					.AddAsync(entity);
 
-    /// <summary>
-    /// Updates an entity in the database.
-    /// </summary>
-    /// <param name="entity">Updated entity data.</param>
-    /// <param name="id"></param>
-    /// <returns></returns>
-    /// <exception cref="ArgumentNullException"></exception>
-    public async Task UpdateAsync(TEntity entity, string id)
-    {
-      if (entity is null)
-      {
-        throw new ArgumentNullException(nameof(entity));
-      }
+				await _context.SaveChangesAsync();
+			}
+			catch (DbUpdateException)
+			{
+				throw;
+			}
 
-      if (!ItemExists(id))
-      {
-        try
-        {
-          _context.Update(entity);
-          await _context.SaveChangesAsync();
-        }
-        catch (DbUpdateConcurrencyException)
-        {
-          throw;
-        }
-      }
-    }
+			return entity;
+		}
 
-    /// <summary>
-    /// Deletes a database entity.
-    /// </summary>
-    /// <param name="id">ID of the entity to be deleted.</param>
-    /// <returns></returns>
-    public async Task DeleteAsync(string id)
-    {
-      var item = await _context
-        .Set<TEntity>()
-        .FirstOrDefaultAsync(e => e.Id == id);
+		/// <summary>
+		/// Updates an entity in the database.
+		/// </summary>
+		/// <param name="entity">Updated entity data.</param>
+		/// <param name="id"></param>
+		/// <returns></returns>
+		/// <exception cref="ArgumentNullException"></exception>
+		public async Task UpdateAsync(TEntity entity, string id)
+		{
+			if (entity is null)
+			{
+				throw new ArgumentNullException(nameof(entity));
+			}
 
-      if (item is not null)
-      {
-        try
-        {
-          _context
-            .Set<TEntity>()
-            .Remove(item);
+			if (!ItemExists(id))
+			{
+				try
+				{
+					_context.Update(entity);
+					await _context.SaveChangesAsync();
+				}
+				catch (DbUpdateConcurrencyException)
+				{
+					throw;
+				}
+			}
+		}
 
-          await _context.SaveChangesAsync();
-        }
-        catch (DbUpdateException)
-        {
-          throw;
-        }
-      }
-    }
+		/// <summary>
+		/// Deletes a database entity.
+		/// </summary>
+		/// <param name="id">ID of the entity to be deleted.</param>
+		/// <returns></returns>
+		public async Task DeleteAsync(string id)
+		{
+			var item = await _context
+				.Set<TEntity>()
+				.FirstOrDefaultAsync(e => e.Id == id);
 
-    protected bool ItemExists(string id)
-    {
-      return _context
-        .Set<TEntity>()
-        .Any(e => e.Id == id);
-    }
+			if (item is not null)
+			{
+				try
+				{
+					_context
+						.Set<TEntity>()
+						.Remove(item);
 
-    protected virtual void Dispose(bool disposing)
-    {
-      if (!_disposed)
-      {
-        if (disposing)
-        {
-          _context.Dispose();
-        }
-      }
+					await _context.SaveChangesAsync();
+				}
+				catch (DbUpdateException)
+				{
+					throw;
+				}
+			}
+		}
 
-      _disposed = true;
-    }
+		protected bool ItemExists(string id)
+		{
+			return _context
+				.Set<TEntity>()
+				.Any(e => e.Id == id);
+		}
 
-    public void Dispose()
-    {
-      Dispose(true);
-      GC.SuppressFinalize(this);
-    }
-  }
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!_disposed)
+			{
+				if (disposing)
+				{
+					_context.Dispose();
+				}
+			}
+
+			_disposed = true;
+		}
+
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+	}
 }
