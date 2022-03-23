28e8b9d1819ec213291d2ab6f9001791eddcfd09
Rasmus Broborg <43502647+RasmusBroborg@users.noreply.github.com>
2022-03-23 10:48:43 +0100

    Add empty api controller for job listings.

---
+++ src/GT.UI/Controllers/JobListingController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GT.UI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class JobListingController : ControllerBase
+	{
+		// GET: api/<JobListingController>
+		[HttpGet]
+		public IEnumerable<string> Get()
+		{
+			return new string[] { "value1", "value2" };
+		}
+
+		// GET api/<JobListingController>/5
+		[HttpGet("{id}")]
+		public string Get(int id)
+		{
+			return "value";
+		}
+
+		// POST api/<JobListingController>
+		[HttpPost]
+		public void Post([FromBody] string value)
+		{
+		}
+
+		// PUT api/<JobListingController>/5
+		[HttpPut("{id}")]
+		public void Put(int id, [FromBody] string value)
+		{
+		}
+
+		// DELETE api/<JobListingController>/5
+		[HttpDelete("{id}")]
+		public void Delete(int id)
+		{
+		}
+	}
+}
