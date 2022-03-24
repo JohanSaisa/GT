4e5762a984b3a1b43538cb3362701a27def8abbc
Rasmus Broborg <43502647+RasmusBroborg@users.noreply.github.com>
2022-03-24 11:05:17 +0100

    Add ExperienceLevel entity. #50.

--- src/GT.Core/FilterModels/ListingFilterModel.cs
+++ src/GT.Core/FilterModels/ListingFilterModel.cs
@@ -2,10 +2,13 @@
 {
 	public class ListingFilterModel : IListingFilterModel
 	{
-		//Includes listing body and title
-		public string FreeText { get; set; }
-		public int MinSalary { get; set; }
-		public int MaxSalary { get; set; }
-
+		// Includes listing title & description
+		public string? FreeText { get; set; }
+		public int? MinSalary { get; set; }
+		public int? MaxSalary { get; set; }
+		// Full Time Equivelent
+		public bool? FTE { get; set; }
+		public string? Location { get; set; }
+		ICollection<string>? ExperienceLevel { get; set; }
 	}
 }
---
+++ src/GT.Data/Data/GTAppDb/Entities/ExperienceLevel.cs
@@ -0,0 +1,9 @@
+﻿namespace GT.Data.Data.GTAppDb.Entities
+{
+	public class ExperienceLevel
+	{
+		public string Id { get; set; }
+		public string Name { get; set; }
+		public ICollection<Listing> Listings { get; set; }
+	}
+}
