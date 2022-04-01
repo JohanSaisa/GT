7fef749422518a7b036f06579df89aeadb50f30c
Johan Säisä <johan.saisa@gmail.com>
2022-04-01 16:01:02 +0200

    Reset partial to bootstrap example. Bug test modal. #94.

--- src/GT.UI/wwwroot/js/sendInquiry.js
+++ src/GT.UI/wwwroot/js/sendInquiry.js
@@ -1,6 +1,6 @@
 ﻿$(document).ready(function () {
 
-	$('#sendInquiry').on('show.bs.modal', function (event) {
+	$('#exampleModal').on('show.bs.modal', function (event) {
 		var button = $(event.relatedTarget) // Button that triggered the modal
 		var recipient = button.data('whatever') // Extract info from data-* attributes
 		// If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
