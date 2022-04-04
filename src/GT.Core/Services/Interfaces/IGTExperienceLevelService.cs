590e45eb404bd72150997f28614d028e71c1a9c5
Rasmus Broborg <rasmus.broborg@icloud.com>
2022-04-04 09:57:37 +0200

    Add GetAll() method for ExperienceLevelService.cs. #96 #99.

--- src/GT.Core/Services/Interfaces/IGTExperienceLevelService.cs
+++ src/GT.Core/Services/Interfaces/IGTExperienceLevelService.cs
@@ -4,6 +4,7 @@ namespace GT.Core.Services.Interfaces
 {
 	public interface IGTExperienceLevelService : IGTService
 	{
+		Task<List<ExperienceLevelDTO>> GetAllAsync();
 		Task<ExperienceLevelDTO> AddAsync(ExperienceLevelDTO dto);
 		Task<bool> ExistsByNameAsync(string name);
 	}
