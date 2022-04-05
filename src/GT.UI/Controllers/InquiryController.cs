f8ce80230630a1d8c673bc1cb3767880d22e3992
Johan Säisä <89136019+JohanSaisa@users.noreply.github.com>
2022-04-05 19:19:15 +0200

    Start draft action method for Create inquiry. #101.

---
+++ src/GT.UI/Controllers/InquiryController.cs
@@ -0,0 +1,26 @@
+﻿using GT.Core.DTO.Impl;
+using GT.Core.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GT.UI.Controllers
+{
+	public class InquiryController : Controller
+	{
+		private readonly IGTListingInquiryService _gtListingInquiryService;
+
+		public InquiryController(IGTListingInquiryService gtListingInquiryService)
+		{
+			_gtListingInquiryService = gtListingInquiryService;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult CreateInquiry(ListingInquiryDTO inquiryDTO, string? signedInUserId = null)
+		{
+			_gtListingInquiryService.AddAsync(inquiryDTO, signedInUserId);
+		}
+	}
+}
