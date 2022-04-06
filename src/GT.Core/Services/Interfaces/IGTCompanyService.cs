e22f1691b2b546090e0d212f968734e4935c122a
Oscar Thorburn <oscar.thorburn@gmail.com>
2022-04-06 12:53:33 +0200

    Start updating IGTCompanyService

--- src/GT.Core/Services/Interfaces/IGTCompanyService.cs
+++ src/GT.Core/Services/Interfaces/IGTCompanyService.cs
@@ -5,6 +5,17 @@ namespace GT.Core.Services.Interfaces
 	public interface IGTCompanyService : IGTService
 	{
 		Task<CompanyDTO> AddAsync(CompanyDTO dto);
+
+		Task<CompanyDTO> GetAsync();
+
+		Task<CompanyDTO> GetByIdAsync(string companyId);
+
 		Task<bool> ExistsByNameAsync(string name);
+
+		string AddCompanyLogo(byte[] file);
+
+		void DeleteCompanyLogo(string companyLogoId);
+
+		Task DeleteAsync(string companyId);
 	}
 }
