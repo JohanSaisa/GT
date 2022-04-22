6a7c037b341182b0e235a2819c3ba783730fe92f
Anton Carlgren <anton.carlgren@hotmail.com>
2022-04-22 14:21:57 +0200

    Re-add missing ListingFilterViewModel from previous commit #148

---
+++ src/GT.UI/Models/ListingFilterViewModel.cs
@@ -0,0 +1,14 @@
+﻿using GT.Core.DTO.Impl;
+using GT.Core.FilterModels.Impl;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace GT.UI.Models
+{
+	public class ListingFilterViewModel
+	{
+		public ListingFilterModel? Filter { get; set; } 
+		public List<ExperienceLevelCheckbox> ExperienceLevels { get; set; } = new();
+		public SelectList? Locations { get; set; } 
+		public bool ExcludeExpiredListings { get; set; }
+	}
+}
